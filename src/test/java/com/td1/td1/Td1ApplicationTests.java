fb00b6e95d35eb1de506b3abb869c98edd2668ee
Meimoune <23040@supnum.mr>
2025-11-14 22:08:28 +0000

    les question de 1 a 4

---
+++ src/test/java/com/td1/td1/Td1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.td1.td1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
